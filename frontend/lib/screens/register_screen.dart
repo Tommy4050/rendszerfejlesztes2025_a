6407574c3d160fb7a485aed5950503f45b8d2253
Tommy4050 <delta4067@gmail.com>
2025-11-13 14:35:02 +0100

    Created the main folder structure(frontend, backend). Added database connection and CRUD(test).

---
+++ frontend/lib/screens/register_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import '../services/api_service.dart';
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  final usernameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  void register() async {
+    final response = await ApiService.registerUser(
+      usernameController.text,
+      emailController.text,
+      passwordController.text,
+    );
+
+
+    if(response.statusCode == 201) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('User registerd!')));
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Error: ${response.body}')));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Register')),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(controller: usernameController, decoration: const InputDecoration(labelText: 'Username')),
+            TextField(controller: emailController, decoration: const InputDecoration(labelText: 'Email')),
+            TextField(controller: passwordController, obscureText: true, decoration: const InputDecoration(labelText: 'Password')),
+            const SizedBox(height: 20),
+            ElevatedButton(onPressed: register, child: const Text('Register')),
+          ],
+        ),
+      ),
+    );
+  }
+}
